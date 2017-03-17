9185c2e05b346133f9b00f2092f4bca3c9ab5d2d
alexianebrao <alexianebrao@wifi-roaming-128-4-82-81.host.udel.edu>
2017-03-16 21:11:56 -0400

    main

---
+++ main.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <stdlib.h>
+#include "Node.hpp"
+#include "LL.hpp"
+using namespace std;
+
+int main(){
+  LL *LL1 = new LL();
+  LL1->push(1);
+  LL1->push(2);
+  LL1->push(3);
+  LL1->push(4);
+  LL1->push(5);
+  LL1->printList();
+  LL1->removekth(7);
+  LL1->removekth(0);
+  LL1->removekth(5);
+  LL1->removekth(0);
+  LL1->printList();
+}//end main
